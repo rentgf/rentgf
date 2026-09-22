e80a56c5a0e15edb595df0f5e80622caca869eed
rentgf <rentgfapp@gmail.com>
2026-09-22 20:01:15 +0530

    fix: update SITE_URL to rentgf.site in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next'
 
-const SITE_URL = 'https://rentgf.in'
+const SITE_URL = 'https://rentgf.site'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const cities = [
